1ecbfeb16d930c95b0b874b37c229d1c53e8e2e4
erikly <erikly2@illinois.edu>
2020-04-27 22:24:24 -0500

    Initial commit

---
+++ app/src/main/java/com/example/pokedex/DetailedPokemonActivity.java
@@ -0,0 +1,30 @@
+package com.example.pokedex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class DetailedPokemonActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_pokemon);
+        button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMain();
+            }
+        });
+    }
+
+    public void openMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/pokedex/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.pokedex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDetails();
+            }
+        });
+    }
+
+    public void openDetails() {
+        Intent intent = new Intent(this, DetailedPokemonActivity.class);
+        startActivity(intent);
+    }
+}
